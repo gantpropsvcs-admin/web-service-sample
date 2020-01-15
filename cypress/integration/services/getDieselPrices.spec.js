f7059300816383bf3b9dc8791d1a793eff56a1ce
gantpropsvcs-admin <58055804+gantpropsvcs-admin@users.noreply.github.com>
2020-01-15 13:23:10 -0700

    add unit tests for get methods

--- cypress/integration/services/getDieselPrices.spec.js
+++ cypress/integration/services/getDieselPrices.spec.js
@@ -1,10 +1,11 @@
 import { baseURL } from '../../../cypress';
 
-describe('API Testing with Cypress', () => {
+describe('Get a specific price form the list', () => {
 
    beforeEach(() => {
-       cy.request(${baseURL}/1).as('dieselprice');
+       cy.request(`${baseURL}/1`).as('dieselprice');
    });
+
 
    it('Validate the status code', () => {
        cy.get('@dieselprice')
@@ -17,4 +18,24 @@ describe('API Testing with Cypress', () => {
            .its('body')
            .should('include', { location_name: 'Shell' });
    });
+});
+
+describe('Get all prices from the list', () => {
+    beforeEach(() => {
+        cy.request(`${baseURL}`).as('dieselprices');
+    });    
+
+    it('Validate the status code', () => {
+        cy.get('@dieselprices')
+            .its('status')
+            .should('equal', 200);
+    });
+    
+    it('Validate more than one item was returned', () => {
+        cy.get('@dieselprices')
+            .its('body')
+            .should(($prices) => {
+                expect($prices).to.have.length(3);
+            });
+    });
 });
